f868bba7664ea9480e1eedea3c218810ec2302a4
Danish Shakeel <danishshakeel54@gmail.com>
2026-02-16 11:43:25 +0100

    chore: bump version to 1.0.1
    Update version in package.json and plugin header, and add v1.0.1 entries to CHANGELOG.md.

--- core-carousel.php
+++ core-carousel.php
@@ -10,7 +10,7 @@
  * Contributors: iamdanih17, immasud
  * License:     GPL2
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
- * Version:     1.0.0
+ * Version:     1.0.1
  * Text Domain: core-carousel
  *
  * @package core-carousel
